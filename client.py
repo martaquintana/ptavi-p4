9f281fb7abf0531d3477abd45e3dba876590a047
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-10-27 15:07:26 +0200

    he puesto que son variables

--- client.py
+++ client.py
@@ -7,18 +7,17 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
-# Constantes. Dirección IP del servidor y contenido a enviar
+# Constantes y Variables. Dirección IP del servidor, puerto y contenido a enviar
 SERVER = 'localhost'
-IP = sys.argv[1]
-PORT = int(sys.argv[2])
-linea = sys.argv[3:len(sys.argv)]
-LINE= ' '.join(linea)
+ip = sys.argv[1]
+port = int(sys.argv[2])
+line= ' '.join(sys.argv[3::])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    my_socket.connect((SERVER, port))
+    print("Enviando:", line)
+    my_socket.send(bytes(line, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
