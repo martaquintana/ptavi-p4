31191f55e986015f4b9a3e67e30a5a3cef7ab364
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-10-28 18:28:36 +0100

    Para que lea bien de la shell

--- client.py
+++ client.py
@@ -9,21 +9,27 @@ import sys
 
 # Constantes y Variables. Dirección IP del servidor, puerto y contenido a enviar
 SERVER = 'localhost'
-ip = sys.argv[1]
-port = int(sys.argv[2])
-#line = ' '.join(sys.argv[3::])
-register = sys.argv[3]
-sip_address = sys.argv[4]
+try:
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+    #line = ' '.join(sys.argv[3::])
+    register = sys.argv[3]
+    sip_address = sys.argv[4]
+    expires = sys.argv[5]
 
+    
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, port))
-    if register == 'register':
-        line = 'REGISTER sip:' + sip_address + ' SIP/2.0\r\n\r\n'
-        print("Enviando:", line, end='')
-        my_socket.send(bytes(line, 'utf-8'))
-        data = my_socket.recv(1024)
-        print('Recibido -- ', data.decode('utf-8'), end='')
-       
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.connect((SERVER, port))
+        if register == 'register':
+            line = 'REGISTER sip:' + sip_address + ' SIP/2.0\r\n' + 'Expires: ' + expires + '\r\n\r\n'
+            print("Enviando:", line, end='')
+            print(expires)
+            my_socket.send(bytes(line, 'utf-8'))
+            data = my_socket.recv(1024)
+            print('Recibido -- ', data.decode('utf-8'), end='')
+    print("Socket terminado.")
+except:
+    print("Usage:client ip puerto" + "register sip_address expires_value")       
 
-print("Socket terminado.")
+
